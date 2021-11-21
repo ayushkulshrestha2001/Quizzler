4ca0988bcb6d8858268586e0bc48435d1cac2790
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-11-21 13:18:48 +0530

    Quiz brain class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,30 +29,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.',
-  // ];
-  // List<bool> answers = [
-  //   false,
-  //   true,
-  //   true,
-  // ];
-  List<Question> questionBank = [
-    Question(
-      q: 'You can lead a cow down stairs but not up stairs.',
-      a: false,
-    ),
-    Question(
-      q: 'Approximately one quarter of human bones are in the feet.',
-      a: true,
-    ),
-    Question(
-      q: 'A slug\'s blood is green.',
-      a: true,
-    ),
-  ];
   int i = 0;
   @override
   Widget build(BuildContext context) {
@@ -64,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[i].questionText,
+                quizBrain.questionBank[i].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -89,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswers = questionBank[i].questionAnswer;
+                bool correctAnswers = quizBrain.questionBank[i].questionAnswer;
                 if (correctAnswers == true) {
                   print('user got it right');
                 } else {
@@ -121,7 +99,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswers = questionBank[i].questionAnswer;
+                bool correctAnswers = quizBrain.questionBank[i].questionAnswer;
                 if (correctAnswers == false) {
                   print('user got it right');
                 } else {
